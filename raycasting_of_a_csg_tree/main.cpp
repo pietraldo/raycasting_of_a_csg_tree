920692b5eb6fffa07b2a8ac4e14b9361f401211f
pietraldo <pietrzakjakub4321@gmail.com>
2024-12-29 16:38:27 +0100

    nice looking shape

--- raycasting_of_a_csg_tree/main.cpp
+++ raycasting_of_a_csg_tree/main.cpp
@@ -38,15 +38,21 @@ int main() {
 
 	glfwSetScrollCallback(window.GetWindow(), scroll_callback);
 
-	const int SPHERE_COUNT = 30;
+	const int SPHERE_COUNT = 6;
 	const int NODE_COUNT = 2 * SPHERE_COUNT - 1;
 
 	Sphere spheres_scene[SPHERE_COUNT];
 
-	const int sphere_render_radius = 10;
-	for (int i = 0; i < SPHERE_COUNT; i++) {
+	//const int sphere_render_radius = 10;
+	/*for (int i = 0; i < SPHERE_COUNT; i++) {
 		spheres_scene[i] = Sphere(vec3(rand() % sphere_render_radius - sphere_render_radius/2, rand() % sphere_render_radius - sphere_render_radius/2, rand() % sphere_render_radius - sphere_render_radius/2), 1.0f);
-	}
+	}*/
+	spheres_scene[0] = Sphere(vec3(1, 0, 0), 1.0f);
+	spheres_scene[1] = Sphere(vec3(-1, 0, 0), 1.0f);
+	spheres_scene[2] = Sphere(vec3(0, 1, 0), 1.0f);
+	spheres_scene[3] = Sphere(vec3(0, -1, 0), 1.0f);
+	spheres_scene[4] = Sphere(vec3(0, 0, 0), 1.5f);
+	spheres_scene[5] = Sphere(vec3(0, 0, 0), 0.5f);
 
 	scene = Scene();
 	scene.SetCamera(Camera(vec3(0, 0, -8)));
@@ -77,7 +83,7 @@ int main() {
 	for (int i = 0; i < SPHERE_COUNT; i++) {
 		nodeArr[i+SPHERE_COUNT-1] = Node{ -1, -1, spheres_scene[i].position.x, spheres_scene[i].position.y, spheres_scene[i].position.z, spheres_scene[i].radius, 0};
 	}
-	int row = 0;
+	/*int row = 0;
 	int col = 0;
 	for (int i = 0; i < SPHERE_COUNT - 1; i++)
 	{
@@ -92,8 +98,13 @@ int main() {
 		{
 			col++;
 		}
-	}
-	
+	}*/
+
+	nodeArr[0] = Node{ 1,10,0,0,0,0,0 };
+	nodeArr[1] = Node{ 2,9,0,0,0,0,1 };
+	nodeArr[2] = Node{ 3,4,0,0,0,0,2 };
+	nodeArr[3] = Node{ 6,5,0,0,0,0,2 };
+	nodeArr[4] = Node{ 8,7,0,0,0,0,2 };
 
 
 	//copy sphere and texture to gpu
